821122b4d750f9be813245366392775a966d11a1
Kaznacheykin <Kaznacheykin@gmail.com>
2021-08-16 20:14:56 +0300

    add new style

--- vue.js
+++ vue.js
@@ -2,9 +2,10 @@ var app = new Vue({
     el: '#app',
     data: {
       notifications: [
-                {title: "Lorem ipsum dolor sit amet 1", description: "Lorem 1 ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada, metus varius varius rutrum, dui odio suscipit magna, non ornare leo tortor id ante. Etiam at sem in lorem ultricies aliquam. ", style: "first-style"},
-                {title: "Lorem ipsum dolor sit amet 2", description: "Lorem 2 ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada, metus varius varius rutrum, dui odio suscipit magna, non ornare leo tortor id ante. Etiam at sem in lorem ultricies aliquam. ", style: "second-style"},
+                {title: "Lorem ipsum dolor sit amet 1", description: "Lorem 1 ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada, metus varius varius rutrum, dui odio suscipit magna, non ornare leo tortor id ante. Etiam at sem in lorem ultricies aliquam. ", style: "second-style"},
+                {title: "Lorem ipsum dolor sit amet 2", description: "Lorem 2 ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada, metus varius varius rutrum, dui odio suscipit magna, non ornare leo tortor id ante. Etiam at sem in lorem ultricies aliquam. ", style: "first-style"},
                 {title: "Lorem ipsum dolor sit amet 3", description: "Lorem 3 ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada, metus varius varius rutrum, dui odio suscipit magna, non ornare leo tortor id ante. Etiam at sem in lorem ultricies aliquam. ", style: "third-style"},
+                {title: "Lorem ipsum dolor sit amet 3", description: "Lorem 3 ipsum dolor sit amet, consectetur adipiscing elit. Fusce malesuada, metus varius varius rutrum, dui odio suscipit magna, non ornare leo tortor id ante. Etiam at sem in lorem ultricies aliquam. ", style: "first-style"},
                       ]
     },
   })
